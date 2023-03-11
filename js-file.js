a4ab87a776e7dc639fc5ee573c02cdcd17a227f6
pcvan01 <111936624+pcvan01@users.noreply.github.com>
2023-03-10 20:50:38 -0700

    Added Preliminary Button Color Functionality

--- js-file.js
+++ js-file.js
@@ -10,12 +10,49 @@ for  (let i=0; i<resolution; i++){
     for (let j=0; j<resolution; j++){
         const rowBoxes = document.createElement('div');
         rowBoxes.classList.add('row-box');
-        rowBoxes.classList.add('inactive');
         rowBoxes.addEventListener('mouseover', cellColor);
         columnBoxes.appendChild(rowBoxes);
     }
     boxContainer.appendChild(columnBoxes);
 }
+
+function cellColor() {
+    this.classList.add('active');
+    this.style.background = 'blue';                                      
+}
+
+// Set up mutually exclusive color buttons
+const exclusiveBlackButton = document.querySelector('#black')
+const exclusiveRainbowButton = document.querySelector('#rainbow')
+const exclusiveEraseButton = document.querySelector('#erase')
+exclusiveBlackButton.addEventListener('click', () => {
+    inactivateButtons();
+    exclusiveBlackButton.classList.add('active-button');
+    exclusiveBlackButton.classList.remove('inactive-button');
+})
+
+exclusiveRainbowButton.addEventListener('click', () => {
+    inactivateButtons();
+    exclusiveRainbowButton.classList.add('active-button');
+    exclusiveRainbowButton.classList.remove('inactive-button');
+})
+
+exclusiveEraseButton.addEventListener('click', () => {
+    inactivateButtons();
+    exclusiveEraseButton.classList.add('active-button');
+    exclusiveEraseButton.classList.remove('inactive-button');
+})
+
+
+//Create a function that inactivates all buttons
+function inactivateButtons() {
+    const activeButtons = document.querySelectorAll('.active-button');
+    for (let jj=0; jj<activeButtons.length; jj++){
+        activeButtons[jj].classList.remove('active-button');
+        activeButtons[jj].classList.add('inactive-button');
+    }
+};
+
 
 // Set up 'Clear' button functionality
 const clear = document.querySelector('#clear');
@@ -24,13 +61,7 @@ clear.addEventListener('click', () => {
     const activeBoxes = document.querySelectorAll('.active');
     for (let ii=0; ii<activeBoxes.length; ii++){
         activeBoxes[ii].classList.remove('active');
-        activeBoxes[ii].classList.add('inactive');
         activeBoxes[ii].style.background = 'white';
     };
 });
 
-function cellColor() {
-    this.classList.remove('inactive');
-    this.classList.add('active');
-    this.style.background = 'blue';                                      
-}
